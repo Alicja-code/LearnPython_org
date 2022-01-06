2245efade200cfb31676b45d5a99e3a7b5280118
Alicja <32157812+AlicjaHelenaM@users.noreply.github.com>
2022-01-06 19:09:54 +0000

    ds

---
+++ data_science_tutorials/2_pandas.py
@@ -0,0 +1,34 @@
+# Import pandas as pd
+import pandas as pd
+
+dict = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+       "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+       "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+       "population": [200.4, 143.5, 1252, 1357, 52.98] }
+
+brics = pd.DataFrame(dict)
+print(brics)
+
+# Set the index for brics
+brics.index = ["BR", "RU", "IN", "CH", "SA"]
+
+# Print out brics with new index values
+print(brics)
+
+# Import the cars.csv data: cars
+cars = pd.read_csv('cars.csv')
+
+# Print out cars
+print(cars)
+
+
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Print out country column as Pandas Series
+print(cars['cars_per_cap'])
+
+# Print out country column as Pandas DataFrame
+print(cars[['cars_per_cap']])
+
+# Print out DataFrame with country and drives_right columns
+print(cars[['cars_per_cap', 'country']])
